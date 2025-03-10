9d0214fd4d7e57fecb1c9db047048e3d5443bf35
mrmorgan-i <misraelkelly@gmail.com>
2025-03-09 21:15:24 -0500

    url-fix

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -1,6 +1,6 @@
 export const siteDetails = {
     siteName: 'Newman Coding Club',
-    siteUrl: 'https://newmancodingclub.vercel.app/',
+    siteUrl: 'https://newmancoding.club/',
     metadata: {
         title: 'Newman Coding Club - Learn, Build, Connect at Newman University Wichita',
         description: 'Join our community at Newman University Wichita to learn coding, build projects, and explore computer science in a supportive environment. No prior experience needed!',
