9ef8a746a139c06161d8d903a95bb33cc6e9fffd
mrmorgan-i <75841431+mrmorgan-i@users.noreply.github.com>
2025-05-12 01:55:57 -0500

    Update events.ts

--- src/data/events.ts
+++ src/data/events.ts
@@ -2,15 +2,6 @@ import { IEvent } from "@/types";
 
 export const events: IEvent[] = [
     {
-        title: "Dessert and Intros: Meet the Newman Coding Club",
-        date: "May 7, 2025",
-        time: "1:00 PM - 2:00 PM",
-        location: "TBD",
-        description: "Join us for a sweet treat and get to know the Newman Coding Club! We'll introduce our team, share our goals, and discuss upcoming events. Perfect for new members!",
-        tags: ["Introductory", "Networking", "Dessert"],
-        registerLink: "https://forms.office.com/r/jFgiGjjmPE"
-    },
-    {
         title: "Guest Appearance",
         date: "TBD",
         time: "6:00 PM - 7:30 PM",
@@ -27,5 +18,14 @@ export const events: IEvent[] = [
         description: "Dive into Python programming! We'll cover syntax, data types, functions, and create simple programs together.",
         tags: ["Python", "Beginner", "Programming"],
         registerLink: "#"
-    }
+    },
+    {
+        title: "TBD",
+        date: "TBD",
+        time: "TBD",
+        location: "TBD",
+        description: "Stay tuned!",
+        tags: ["Anticipate"],
+        registerLink: "#"
+    },
 ];
