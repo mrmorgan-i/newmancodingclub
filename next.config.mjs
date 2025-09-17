b814f5dd9bd8224627e97df54de22f3ff21426db
mrmorgan-i <misraelkelly@gmail.com>
2025-09-16 21:50:37 -0500

    update configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        qualities: [25, 50, 75, 100]
+    }
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,32 @@ const config: Config = {
         "foreground-accent": "var(--foreground-accent)",
         "hero-background": "var(--hero-background)",
       },
+      animation: {
+        'fade-in-up': 'fadeInUp 0.9s cubic-bezier(0.25, 0.46, 0.45, 0.94)',
+        'slide-in-left': 'slideInLeft 1s cubic-bezier(0.25, 0.46, 0.45, 0.94)',
+      },
+      keyframes: {
+        fadeInUp: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(100px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+        slideInLeft: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(-50px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+        },
+      },
     },
   },
   plugins: [],
