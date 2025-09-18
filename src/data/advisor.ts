822d1b922eea05e6b48dceaca0bd5770d0c8462e
Thatcher McClure <thatchermcclure@gmail.com>
2025-09-18 16:18:31 -0500

    feat: middle align leadership & new advisor
    Middle aligned the leadership component using flex, only issue is that at certain widths it
    can be uneven with other cards height's
    
    Added Cochran as advisor

--- src/data/advisor.ts
+++ src/data/advisor.ts
@@ -1,9 +1,18 @@
 import { IAdvisor } from "@/types";
 
-export const advisor: IAdvisor[] = [{
+export const advisor: IAdvisor[] = [
+  {
     name: "Dr. Robert Norman",
     role: "Advisor",
-    bio: "Professor of Computer Science at Newman University.",
+    bio: "Professor of Computer Science at Newman University",
     avatar: "/images/leadership/advisor.jpg",
     contact: "normanr@newmanu.edu",
-}]
+  },
+  {
+    name: "Dr. David Cochran",
+    role: "Advisor",
+    bio: "Dean of the School of Business & Technology",
+    avatar: "/images/leadership/advisor2.jpg",
+    contact: "cochrand@newmanu.edu",
+  },
+];
